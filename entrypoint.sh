51fbc5014d003c2a6e921855d67add5b5d5df20a
Florian <herzog-florian@gmx.de>
2016-11-16 21:33:27 +0100

    fix setgid bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,8 +16,8 @@ syslog = no
 debug = ${DEBUG_LEVEL}
 options = NO_SSLv2
 
-setuid     = nobody
-setgid     = nobody
+setuid = nobody
+setgid = nogroup
 
 socket=l:TCP_NODELAY=1
 socket=r:TCP_NODELAY=1
